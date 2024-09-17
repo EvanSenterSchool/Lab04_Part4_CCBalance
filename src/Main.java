7b0c68f581eda43d455fc2fd1b06ab099d2633e4
Evan Senter (614925) <614925@swl.local>
2024-09-17 08:35:51 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
+        double creditBalance = 5000;
+        double interestRate = 0.17;
+        double updatedBalance;
+
+        updatedBalance = creditBalance + (creditBalance * interestRate);
+        System.out.println("The interest for first month is " + updatedBalance);
+        updatedBalance = updatedBalance + (updatedBalance * interestRate);
+        System.out.println("The interest for second month is " + updatedBalance);
+     }
 }
